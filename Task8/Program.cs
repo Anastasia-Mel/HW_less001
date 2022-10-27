566a688f07eeb282470f09c2fe0efba8fb588a57
Anastasia_Mel <melnik2775@gmail.com>
2022-10-28 01:07:38 +0300

    Task8 for Homework-Seminar1

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿//Принимает число (N) и выдает
+//все четные (num%2=0) от 1 до N
+
+Console.WriteLine("Enter positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int rest = Convert.ToInt32(num % 2);
+int index = 2;
+
+while(index <= num)
+{
+    if(rest == 0);
+    Console.Write($"{index}, ");
+    index += 2;
+}
