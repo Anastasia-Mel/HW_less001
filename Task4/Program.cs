34a1ea72116d48f2aafaab05f89459803ec4b5dc
Anastasia_Mel <melnik2775@gmail.com>
2022-10-27 23:53:44 +0300

    Task4 from Homework-Seminar1

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿//Принимает три числа и выдает
+//максимальное из этих чисел
+
+Console.WriteLine("Enter number one: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number two: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number three: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 >= num2 && num1 > num3)
+{
+    Console.WriteLine($"Max number: {num1}.");
+}
+else if(num2 > num1 && num2 >= num3)
+{
+    Console.WriteLine($"Max number: {num2}.");
+}
+else if(num3 >= num1 && num3 > num2)
+{
+    Console.WriteLine($"Max number: {num3}.");
+}
+else if(num1 == num2 && num1 == num3 && num2 == num3)
+{
+    Console.WriteLine("The numberts are equal!");
+}
