9f04b147234613b92ee0a2833ad7d467e3577738
Anastasia_Mel <melnik2775@gmail.com>
2022-10-28 00:07:59 +0300

    Task6 from Homework-Seminar1

---
+++ Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿//Принимает число и выдает,
+//является ли число четным (num%2=0)
+
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int rest = Convert.ToInt32(num % 2);
+
+if(rest == 0)
+{
+    Console.WriteLine("The number is even.");
+}
+else
+{
+    Console.WriteLine("The number is odd.");
+}
